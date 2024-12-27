86640a7cd25bf1cacafb58a3743bf837b929c61f
dahn0209 <dahn0209@gmail.com>
2024-12-27 10:20:50 -0500

    added backend

---
+++ backend_project_2/src/main/java/com/example/backend_project_2/BackendProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.backend_project_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProject2Application.class, args);
+	}
+
+}
---
+++ backend_project_2/src/test/java/com/example/backend_project_2/BackendProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
