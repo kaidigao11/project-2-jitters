387ade5a53aa0160a8459b8a1bea3854f30ac38f
Kaidi1 <winson096@gmail.com>
2024-12-30 15:26:42 -0500

    Added folders for entity, controller, repository and service. Added boiler plate for controller class

---
+++ backend_project_2/src/main/java/com/example/backend_project_2/controller/Project2Controller.java
@@ -0,0 +1,21 @@
+package com.example.backend_project_2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Project2Controller {
+    
+
+
+
+    public Project2Controller(){
+        
+    }
+
+
+
+
+
+
+
+}
