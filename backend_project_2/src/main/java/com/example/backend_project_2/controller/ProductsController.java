3a43fe93c671f321603d1617faea8ec4a750c003
kaidigao11 <kaidigao11@gmail.com>
2024-12-31 14:29:42 -0500

    Added api mapping and added getAllProducts route

--- backend_project_2/src/main/java/com/example/backend_project_2/controller/ProductsController.java
+++ backend_project_2/src/main/java/com/example/backend_project_2/controller/ProductsController.java
@@ -1,15 +1,29 @@
 package com.example.backend_project_2.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend_project_2.entity.OrderDetail;
+import com.example.backend_project_2.entity.Product;
+import com.example.backend_project_2.service.ProductService;
+
 @RestController
+@RequestMapping("/api/products")
 public class ProductsController {
     
+    @Autowired
+    private ProductService productService;
 
-
-    
-    public ProductsController(){
-        
+    // get all products 
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllOrderDetails() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
     }
 
 
--- backend_project_2/src/main/java/com/example/backend_project_2/service/ProductService.java
+++ backend_project_2/src/main/java/com/example/backend_project_2/service/ProductService.java
@@ -1,14 +1,23 @@
 package com.example.backend_project_2.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.backend_project_2.entity.OrderDetail;
+import com.example.backend_project_2.entity.Product;
 import com.example.backend_project_2.repository.ProductRepository;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+    
+    // get all products 
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 
 
 }
